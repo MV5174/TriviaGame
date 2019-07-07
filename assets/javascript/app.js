123546b566abe98075d362c95058c2888bd6c0ab
Michael Vega <ichigo509@yahoo.com>
2019-07-06 21:23:19 -0400

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//Global Variables
+
+//Array with Questions
+
+//Array with Answers
+
+//Timer for questions
+
+//If the player selects the correct answer, show a screen congratulating them for choosing the right option.
+
+//After a few seconds, display the next question
+
+//If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+
+//If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+
+//On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
